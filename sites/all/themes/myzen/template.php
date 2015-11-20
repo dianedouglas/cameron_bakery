e49265993004b75ffcb8021f6b3f89a87d6ab65d
JackMackerel <ludichrislyts@gmail.com>
2015-11-19 23:49:24 -0800

    override breadcrumb theme function

--- sites/all/themes/myzen/template.php
+++ sites/all/themes/myzen/template.php
@@ -172,3 +172,15 @@ function myzen_preprocess_node(&$variables){
         kpr($variables);
     }
 }
+function myzen_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $title = drupal_get_title();
+    $output .= '<div class="breadcrumb">' . implode(' » ', $breadcrumb) . ' » ' . $title .'</div>';
+    return $output;
+  }
+}
