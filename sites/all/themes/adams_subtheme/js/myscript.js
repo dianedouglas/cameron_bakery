077aee6c1c8b22f2815db1556838fc90046e0aba
Adam Ross Russell <adamrr724@yahoo.com>
2016-05-06 12:06:44 -0700

    add jquery animations to the subtheme

---
+++ sites/all/themes/adams_subtheme/js/myscript.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  $('#name-and-slogan').addClass('animated fadeIn');
+  $('.node-article').addClass('animated fadeInUp');
+  $('#logo').addClass('animated tada')
+});
