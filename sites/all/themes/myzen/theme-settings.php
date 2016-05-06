a340061a5f043792b57de372dc478d96b201192d
Joe Karasek <joseph.karasek@gmail.com>
2016-05-06 10:08:07 -0700

    add new color and main menu layout

--- sites/all/themes/myzen/theme-settings.php
+++ sites/all/themes/myzen/theme-settings.php
@@ -7,7 +7,7 @@
  * @param $form_state
  *   A keyed array containing the current state of the form.
  */
-function STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+function myzen_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
@@ -16,10 +16,10 @@ function STARTERKIT_form_system_theme_settings_alter(&$form, &$form_state, $form
   // Create the form using Forms API: http://api.drupal.org/api/7
 
   /* -- Delete this line if you want to use this setting
-  $form['STARTERKIT_example'] = array(
+  $form['myzen_example'] = array(
     '#type'          => 'checkbox',
-    '#title'         => t('STARTERKIT sample setting'),
-    '#default_value' => theme_get_setting('STARTERKIT_example'),
+    '#title'         => t('myzen sample setting'),
+    '#default_value' => theme_get_setting('myzen_example'),
     '#description'   => t("This option doesn't do anything; it's just an example."),
   );
   // */
