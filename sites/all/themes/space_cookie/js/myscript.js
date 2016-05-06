6db31439ea0fd25bc26e0713813f3a117e691978
Joe Karasek <joseph.karasek@gmail.com>
2016-05-06 12:24:58 -0700

    add javascript to custom theme

--- sites/all/themes/space_cookie/js/myscript.js
+++ sites/all/themes/space_cookie/js/myscript.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function() {
   jQuery('a').hover(
     function() {
-      jQuery(this).addClass('animated flash');
+      jQuery(this).addClass('animated flash infinite');
       console.log("Yo");
     },
     function() {
-      jQuery('#header').removeClass('animated flash');
+      jQuery(this).removeClass('animated flash infinite');
       console.log("Yo out");
     }
   );
