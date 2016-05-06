9dbbf990c3f1cc7c109f5707be381b2f98a9fdac
Marika <mballely@gmail.com>
2016-05-06 13:16:51 -0700

    add required javascript

--- sites/all/themes/custom_theme/html.tpl.php
+++ sites/all/themes/custom_theme/html.tpl.php
@@ -53,11 +53,14 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
+    <div class="container">
+        <div id="skip-link">
+          <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+        </div>
+        <?php print $page_top; ?>
+        <?php print $page; ?>
+        <?php print $page_bottom; ?>
+    </div>
+
 </body>
 </html>
---
+++ sites/all/themes/custom_theme/js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    $('#navigation li').wrap( "<div class='custom_theme-menu-item'></div>" );
+
+    $('#header').click(function() {
+        alert("Save Ferris");
+    });
+
+});
