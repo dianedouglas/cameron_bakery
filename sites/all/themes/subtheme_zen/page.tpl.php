45d2dde060ec4a9ea90531c12efcf11723b72bb3
milleraundra <miller.aundra@gmail.com>
2016-05-06 13:14:25 -0700

    update page layout and add another image to image slider.

--- sites/all/themes/subtheme_zen/page.tpl.php
+++ sites/all/themes/subtheme_zen/page.tpl.php
@@ -50,32 +50,8 @@
 
   </header>
 
-  <?php if ($page['slider']): ?>
-   <div id="slider">
-     <?php print render($page['slider']); ?>
-   </div> <!-- End Featured Slider-->
- <?php endif; ?>
-
   <div id="main">
 
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
 
     <div id="navigation">
 
@@ -104,6 +80,31 @@
 
     </div>
 
+    <?php if ($page['slider']): ?>
+        <div id="slider">
+            <?php print render($page['slider']); ?>
+        </div> <!-- End Featured Slider-->
+    <?php endif; ?>
+
+
+    <div id="content" class="column" role="main">
+        <?php print render($page['highlighted']); ?>
+        <?php print $breadcrumb; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+            <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+    </div>
     <?php
       // Render the sidebars to see if there's anything in them.
       $sidebar_first  = render($page['sidebar_first']);
