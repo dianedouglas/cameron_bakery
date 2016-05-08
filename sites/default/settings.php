e6a594bc3f052a1b771b8a325ed7be1ed25afc64
mollieboots <mollie.bootsma@gmail.com>
2016-05-08 15:55:09 -0700

    fix camerons module syntax errors

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,15 +213,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'bakery',
       'username' => 'cameron',
       'password' => 'cameron',
       'host' => '127.0.0.1',
-      'port' => '8889',
+      'port' => '3306',
       'driver' => 'mysql',
       'prefix' => '',
     ),
