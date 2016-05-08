154b4ceb782a99594f946d5303a77b37246e2db1
Blake Scott <blakerscott@gmail.com>
2016-05-08 16:40:03 -0700

    add additional styling to zen subtheme

--- sites/all/themes/subtheme/js/script.js
+++ sites/all/themes/subtheme/js/script.js
@@ -15,12 +15,12 @@
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
-  attach: function(context, settings) {
+    attach: function(context, settings) {
 
-    // Place your code here.
+       // Place your code here.
 
-  }
-};
+     }
+   };
 
 
 })(jQuery, Drupal, this, this.document);
