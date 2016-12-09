35717976f9fcfb117cd000bb0c8606c91c02d54f
Seth Kendall <sethpkendall@gmail.com>
2016-12-09 11:10:50 -0800

    style about page and add some js

---
+++ sites/all/themes/sethzen/js/myscripts.js
@@ -0,0 +1,11 @@
+// $(function() {
+//     $('.field-name-field-photo').click(function() {
+//
+//     });
+// });
+jQuery(document).ready(function($) {
+  $('.field-name-field-photo').click(function(){
+    $('.field-name-field-photo').slideToggle();
+    $('.field-name-field-photo2').slideToggle();
+  });
+});
