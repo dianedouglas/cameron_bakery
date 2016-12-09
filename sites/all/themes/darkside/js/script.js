54ca7f19a3e8caa656f362587b46289ebd3b0f4e
martin cartledge <martincartledge@icloud.com>
2016-12-09 14:36:19 -0800

    add click function to #site-slogan in lightside theme

--- sites/all/themes/darkside/js/script.js
+++ sites/all/themes/darkside/js/script.js
@@ -18,8 +18,6 @@
   Drupal.behaviors.my_custom_behavior = {
     attach: function (context, settings) {
 
-      // Place your code here.
-
     }
   };
 
---
+++ sites/all/themes/lightside/js/scripts.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+  $('#site-slogan').click(function() {
+      alert('The cookie monster says yum!');
+  });
+});
