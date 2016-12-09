6a728747a0be28cc37abe76fe80d80578209aeea
Molly LeCompte <mollyklecompte@gmail.com>
2016-12-09 11:54:31 -0800

    add login region, style other regions.

--- sites/all/themes/cameron/template.php
+++ sites/all/themes/cameron/template.php
@@ -1,5 +1,5 @@
 <?php
 
 function cameron_preprocess_html(&$variables) {
-   drupal_add_css('https://fonts.googleapis.com/css?family=Lato|Molle:400i', array('group' => CSS_THEME));
+   drupal_add_css('https://fonts.googleapis.com/css?family=Crete+Round|PT+Serif|Lato|Molle:400i', array('group' => CSS_THEME));
 }
