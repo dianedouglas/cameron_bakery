4875fe9f328585d98376c8669c81eaa3d8315577
ayana powell <ayanaaap@gmail.com>
2016-12-09 17:37:49 -0800

    final edits to custom theme

---
+++ sites/all/themes/cameron_theme/page.tpl.php
@@ -0,0 +1,19 @@
+<div id="header" class="custom-class">
+  <?php print render($page['header']); ?>
+</div>
+
+<div id="top" class="custom-class">
+  <?php print render($page['top']); ?>
+</div>
+
+<div id="content" class="custom-class">
+  <?php print render($page['content']); ?>
+</div>
+
+<div id="announcements" class="custom-class">
+  <?php print render($page['announcements']); ?>
+</div>
+
+<div id="footer" class="custom-class">
+  <?php print render($page['footer']); ?>
+</div>
