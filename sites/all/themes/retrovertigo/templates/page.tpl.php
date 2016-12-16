98287e4e1884a87db4bf8562fdf070c232daceec
Tim <tim.bourgault@gmail.com>
2016-12-16 09:36:04 -0800

    update and finalize the styling for retrovertigo theme.

--- sites/all/themes/retrovertigo/templates/page.tpl.php
+++ sites/all/themes/retrovertigo/templates/page.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Bartik's theme implementation to display a single Drupal page.
+ * Retrovertigo's theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
  * can be found in the html.tpl.php template normally located in the
@@ -14,7 +14,7 @@
  * - $base_path: The base URL path of the Drupal installation. At the very
  *   least, this will always default to /.
  * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
+ *   or themes/retrovertigo.
  * - $is_front: TRUE if the current page is the front page.
  * - $logged_in: TRUE if the user is registered and signed in.
  * - $is_admin: TRUE if the user has permission to access administration pages.
@@ -81,7 +81,7 @@
  * @see template_preprocess()
  * @see template_preprocess_page()
  * @see template_process()
- * @see bartik_process_page()
+ * @see retrovertigo_process_page()
  * @see html.tpl.php
  */
 ?>
