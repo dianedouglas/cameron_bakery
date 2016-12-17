94e6e455365cec6fa9693825fe0eb324833aa2fc
Tim <tim.bourgault@gmail.com>
2016-12-16 18:18:53 -0800

    start implementing javascript.

--- sites/all/themes/retrovertigo/js/scripts.js
+++ sites/all/themes/retrovertigo/js/scripts.js
@@ -1,5 +1,5 @@
-// jQuery(document).ready(function($) {
-//   $('#navigation h2').click(function(){
-//     alert('I am an h2!');
-//   });
-// });
+jQuery(document).ready(function($) {
+  $('#activator').click(function() {
+    
+  });
+});
